d5eb8f75e14f72b04f7b0ac679d6b4e1c38bb17b
Javy0119 <138019998+Javy0119@users.noreply.github.com>
2025-11-05 01:10:32 +0000

    Updated qr code

--- qr_generator.py
+++ qr_generator.py
@@ -1,7 +1,9 @@
 import qrcode
 
 # Data to encode
-data = "https://javy-scratchspace.github.io/javy0119.github.io/"
+data = "https://javy-scratchspace.github.io/javy0119_portfolio.io/"
+# data = "https://docs.google.com/forms/d/e/1FAIpQLSfeStGyBQhb8gy1TgGFZXIXBIhJjWHej_AbQpd2pEVyp9g6nQ/viewform?usp=header"
+# data = "https://forms.gle/T9BafCG3X5s96iNr7"
 
 # Generate QR code
 qr = qrcode.QRCode(
@@ -17,4 +19,5 @@ qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color="white")
 
 # Save to a file
+# img.save("JACC_website_qr.png")
 img.save("JACC_website_qr.png")
