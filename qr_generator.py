39765c2381c204c8de4c46989d48d688c8045771
Javy0119 <138019998+Javy0119@users.noreply.github.com>
2026-03-06 21:30:12 +0000

    Made changes to repo

--- qr_generator.py
+++ qr_generator.py
@@ -1,9 +1,12 @@
 import qrcode
 
+
 # Data to encode
 data = "https://javy-scratchspace.github.io/javy0119_portfolio.io/"
+data = "https://docs.google.com/forms/d/e/1FAIpQLSdK_ws9xDJZ0xMY1EZmGHOjqLOwfCISUlM25Q5ZtGs-ysaGYg/viewform?usp=dialog"
 # data = "https://docs.google.com/forms/d/e/1FAIpQLSfeStGyBQhb8gy1TgGFZXIXBIhJjWHej_AbQpd2pEVyp9g6nQ/viewform?usp=header"
 # data = "https://forms.gle/T9BafCG3X5s96iNr7"
+# data = "https://docs.google.com/forms/d/e/1FAIpQLSfEEtkknZluG2B0uhsGJKChme2k8YTzCytiDKGTDGkEGjfI6w/viewform?usp=dialog"
 
 # Generate QR code
 qr = qrcode.QRCode(
